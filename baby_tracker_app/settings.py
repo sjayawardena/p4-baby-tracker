7ef8c145f34b85384a2f2b3551ae2bda493c9091
Simon Jayawardena <simonjayawardena@hotmail.co.uk>
2024-03-19 17:59:54 +0000

    Finish wire up allauth, add crispy forms

--- baby_tracker_app/settings.py
+++ baby_tracker_app/settings.py
@@ -53,6 +53,10 @@ INSTALLED_APPS = [
     # Apps
     'nappy_changes',
     'home',
+    
+    # Other
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 SITE_ID = 1
@@ -68,13 +72,17 @@ MIDDLEWARE = [
     'allauth.account.middleware.AccountMiddleware',
 ]
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
 ROOT_URLCONF = 'baby_tracker_app.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates')    
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth')    
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -84,6 +92,10 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field'
+            ]
         },
     },
 ]
