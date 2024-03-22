01cd9d998b17f397916c14704e711d179fb62f50
Simon Jayawardena <simonjayawardena@hotmail.co.uk>
2024-03-22 18:05:55 +0000

    Tweak cards in nappy changes list

--- nappy_changes/views.py
+++ nappy_changes/views.py
@@ -2,18 +2,16 @@ from django.views.generic import CreateView, ListView
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models import NappyChange
+from .models import NappyChange, User
 
 from .forms import NappyChangeForm
 
-
-
-class NappyChangesList(ListView):
+class NappyChangesList(LoginRequiredMixin, ListView):
     """View list of recent nappy changes"""
     template_name = 'nappy_changes/nappy_changes_list.html'
     model = NappyChange
     context_object_name = 'nappy_changes_list'
-
+    
 
 class AddNappyChange(LoginRequiredMixin, CreateView):
     """
