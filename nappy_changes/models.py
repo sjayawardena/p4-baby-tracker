a724733d632155b3ef698bf9a815dfc35a24a0f8
Simon Jayawardena <simonjayawardena@hotmail.co.uk>
2024-04-07 19:31:41 +0000

    Final button styling, favicon, add wet+dirty nappy type

--- nappy_changes/models.py
+++ nappy_changes/models.py
@@ -9,7 +9,7 @@ class NappyChange(models.Model):
     A model to enter and edit nappy changes
     """
 
-    NAPPY_CONTENTS = (("wet", "Wet"), ("dirty", "Dirty"), ("wet-and-dirty", "Wet and Dirty"), ("nothing", "Nothing"))
+    NAPPY_CONTENTS = (("wet", "Wet"), ("dirty", "Dirty"), ("wet+dirty", "Wet+Dirty"), ("nothing", "Nothing"))
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="nappy_changes_list"
     )
