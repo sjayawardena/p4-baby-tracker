6ea12c91a59bbb713c26c6b665ad187e05788fcf
Simon Jayawardena <simonjayawardena@hotmail.co.uk>
2024-04-08 10:32:09 +0000

    Run collectstatic

---
+++ staticfiles/js/add_feed.js
@@ -0,0 +1,22 @@
+/** Defining global variables */
+
+const dropdown = document.querySelector("#id_feed_type"); //variable for dropdown feedtype selector
+const breast = document.querySelector("#div_id_breast_feed_time_minutes"); //variable for breast feed duration input
+const formula = document.querySelector("#div_id_formula_amount_ml"); //variable for formula feed amount input
+
+/** Event listener to change feed amount input based on selection of breast or formula feed */
+
+dropdown.addEventListener("change", event => {
+  if (event.target.value === 'breast') {
+    breast.style.display = 'block'
+    formula.style.display = 'none'
+  }
+  else if (event.target.value === 'formula') {
+    breast.style.display = 'none'
+    formula.style.display = 'block'
+  }
+  else {
+    breast.style.display = 'none'
+    formula.style.display = 'none'
+  }
+})
---
+++ staticfiles/js/edit_feed.js
@@ -0,0 +1,20 @@
+/** Defining global variables */
+
+const dropdown = document.querySelector("#id_feed_type"); //variable for dropdown feedtype selector
+const breast = document.querySelector("#div_id_breast_feed_time_minutes"); //variable for breast feed duration input
+const formula = document.querySelector("#div_id_formula_amount_ml"); //variable for formula feed amount input
+
+/** Event listener to change feed amount input based on selection of breast or formula feed */
+
+if (dropdown.value === 'breast') {
+    breast.style.display = 'block'
+    formula.style.display = 'none'
+  }
+  else if (dropdown.value === 'formula') {
+    breast.style.display = 'none'
+    formula.style.display = 'block'
+  }
+  else {
+    breast.style.display = 'none'
+    formula.style.display = 'none'
+  }
