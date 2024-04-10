f1c17f5abe02064fc19fbf2da2f9623252df33a4
Simon Jayawardena <simonjayawardena@hotmail.co.uk>
2024-04-10 12:17:45 +0000

    Set debug to False in settings.py

--- baby_tracker_app/settings.py
+++ baby_tracker_app/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "8000-sjayawarden-p4babytrack-qnjd3hjlur6.ws-eu110.gitpod.io",
